84ea5fc84820d6d5d38b54298006d302ed015cb7
semimaks <semimaks@mail.ru>
2023-02-28 00:57:06 +0300

    Создан проект и директория для изображений

---
+++ main.py
@@ -0,0 +1,29 @@
+# https://russianblogs.com/article/8723583618/
+
+background = 'img/background.jpg'
+mouse_img = 'img/25px.png'
+
+import pygame
+
+from pygame.locals import *
+
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480), 0, 32)
+pygame.display.set_caption('FOREST')
+background = pygame.image.load(background)
+mouse_cursor = pygame.image.load(mouse_img)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+
+    screen.blit(background, (0, 0))
+    x, y = pygame.mouse.get_pos()
+    x -= mouse_cursor.get_width() / 2
+    y -= mouse_cursor.get_height() / 2
+
+    screen.blit(mouse_cursor, (x, y))
+    pygame.display.update()
